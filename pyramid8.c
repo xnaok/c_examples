c875a8e27aa6d8a609df209d243b9b6868bd5cc8
sh0layere9 <carrera911992@gmail.com>
2022-03-06 23:58:32 +0900

    Added pyramid8.c

---
+++ pyramid8.c
@@ -0,0 +1,7 @@
+// program to generate Inverted full pyramid of *
+#include <stdio.h>
+int main() {
+    int rows, i, j, space;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+}
